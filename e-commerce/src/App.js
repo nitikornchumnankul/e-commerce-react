0bd3d4bc5be666e4413419d93486ac003cb4b8cc
nitikornchumnnakul <nitikornchumnnakul@gmail.com>
2021-10-19 01:21:57 +0700

    Hello World on web page

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+// import AddProduct from './components/AddProduct';
+// import Cart from './components/Cart';
+// import Login from './components/Login';
+// import ProductList from './components/ProductList';
+import Context from './Context';
+// import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  
+  render() {
+    return (
+      <Context.Provider>
+      <h1>Hello World</h1>
+      </Context.Provider>
+    )
+  }
 }
-
-export default App;
---
+++ e-commerce/src/components/AddProduct.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function AddProduct(){
+    return <>AddProduct</>
+}
